55e15c9bb61031daf4d9372f0b944c92fa3e30cf
tomwoen <thomas.owen@qa.com>
2018-04-10 17:11:48 +0100

    Added some more functions

--- javascript/codechallenge.js
+++ javascript/codechallenge.js
@@ -1,2 +1,49 @@
-var variableString = "Welcome to my world";
+var square = function sqfun() {
+    
+    var p1 = document.getElementById("input").value;
+    document.getElementById("squarep").append(p1*p1);
+    
+}
+
+//console.log(square(3));
+
+var sum = function sumfun(p1, p2, p3) {
+    
+    document.getElementById("sump").append(p1 + p2 + p3);
+    
+}
+
+//console.log(sum(2,4,6));
+
+var object1 = {
+    
+    name: "Tom",
+    occupation: "Boss",
+    age: "333",
+    
+}
+
+var object = function objfun() {
+    
+    var x = document.getElementById("pers");
+    
+    x.append(object1.name+"\n");
+    
+    x.append(object1.occupation);
+    x.append("<br>");
+    x.append(object1.age);
+    x.append("<br>");
+    
+    object1.name = "Jeff";
+    object1.occupation = "Undercover Policeman";
+    object1.age = "22";
+    
+    x.append(object1.name+"\n");
+    
+    x.append(object1.occupation);
+    x.append("<br>");
+    x.append(object1.age);
+    
+    
+}
 
